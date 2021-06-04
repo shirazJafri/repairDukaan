1df663b3edb06c156b39ab369fee3121067a7f3a
Syed Muhammad Hamza Raza <49732070+HamzaRaza786@users.noreply.github.com>
2021-06-05 03:19:46 +0500

    Add files via upload
    Added Splash Screen

---
+++ screens/Splashscreen.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { View } from 'react-native';
+import { connect } from 'react-redux';
+import { getUserInfo } from '../redux/user/userActions';
+
+function SplashScreen({userState, authState, getInfo, navigation}) {
+    useEffect(() => {
+        getInfo(authState.token)
+    }, [])
+
+    if (!userState.loading && userState.userInfo)
+        navigation.navigate('DrawerNavigation')
+
+    return (
+        <View style = {styles.activity}>
+            <ActivityIndicator size="large" color="#00ff00" />
+        </View>
+    )
+}
+const mapStateToProps = state => {
+    return {
+        authState: state.auth,
+        userState: state.user
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getInfo: (tokenVal) => dispatch(getUserInfo(tokenVal))
+    }
+}
+
+const styles = StyleSheet.create({
+    activity: {
+        flex: 1,
+        justifyContent: "center"
+      }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SplashScreen);
