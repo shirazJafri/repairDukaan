64e8f81b9ac56c6bccac3195019ed44211e71a86
Syed Muhammad Hamza Raza <49732070+HamzaRaza786@users.noreply.github.com>
2021-06-05 03:15:50 +0500

    Update Inprogress.jsx

--- screens/Inprogress.jsx
+++ screens/Inprogress.jsx
@@ -4,22 +4,23 @@ import Constants from 'expo-constants';
 import axios from 'axios';
 const Inprogress = ({route,navigation}) => {
     const repair_id = route.params.repair_id;
+  const [move,setMove] = useState(false);
     const handleArrived = async() =>{
         // console.log("hello")
           await axios.post('https://enigmatic-mesa-42065.herokuapp.com/user/status',{repair_id}).then((res) =>{
              if(res.data.status == "Completed"){
-             navigation.navigate('Map');
-           }
+              setMove(true)
+            }
           }).catch((err) => {
               console.log(err);
           })
         }
-       useEffect(() => {
-         var handle=setInterval(handleArrived,1000);    
-         return ()=>{
-           clearInterval(handle);
-         }
-       })
+        useEffect(() => {
+          var handle=setInterval(handleArrived,1000);    
+        })
+        if(move){
+          navigation.navigate("Map",{repair_id:repair_id})
+        }
        return(
            <View style={styles.container}>
                <Text>Your Repair has Started</Text>
