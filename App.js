3ecdd90e5702c72930ef3ff142c5ffd2a967ed79
Syed Muhammad Hamza Raza <49732070+HamzaRaza786@users.noreply.github.com>
2021-06-05 03:18:39 +0500

    Update App.js

--- App.js
+++ App.js
@@ -8,44 +8,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import UpdatePassword from "./screens/UpdatePassword";
 import store from './redux/store';
 import {Provider} from 'react-redux'
+import {LogBox} from 'react-native'
 import RatesScreen from './screens/RatesScreen';
 import Contact from "./screens/Contact"
+import SplashScreen from './screens/Splashscreen'
 import Report from './screens/Report';
-//import Home from './screens/Home';
 import HowToUse from './screens/HowToUse'
 import DrawerNagivation from './screens/DrawerNagivation';
-//import Map from './screens/Map';
-//import Tracker from './screens/Tracker';
-//import Inprogress from './screens/Inprogress';
-
+LogBox.ignoreAllLogs(true);
 const Stack = createStackNavigator();
-
-
-
 export default function App() {
   return (
     <NavigationContainer>
       <Provider store = {store}>
         <Stack.Navigator screenOptions = {{headerShown: false}}>
           <Stack.Screen name = "LogIn" component = {LogInForm} />
-          {/*<Stack.Screen name = "Profile" component = {Profile}/>
-          <Stack.Screen name="Map" component = {Map} />
-          <Stack.Screen name="Tracker" component = {Tracker} />
-  <Stack.Screen name="InProgress" component = {Inprogress} />*/}
+          <Stack.Screen name = "SplashScreen" component = {SplashScreen}/>
           <Stack.Screen name = "RegisterScreen" component = {RegisterScreen} />
           <Stack.Screen name = "DrawerNavigation" component = {DrawerNagivation} options =  {{title: 'REPAIR DUKAAN', headerLeft: () => null}}/>
-          {/*<Stack.Screen name = "DrawerNavigation" component = {DrawerNagivation} />
-          <Stack.Screen name = "Profile" component = {Profile}/>
-          <Stack.Screen name = "UpdatePassword" component = {UpdatePassword} />
-          <Stack.Screen name = "UpdateScreen" component = {UpdateProfile} />
-          <Stack.Screen name= "ContactUs" component = {Contact} />
-          <Stack.Screen name = "Report" component = {Report} />
-          <Stack.Screen name = "Home" component = {Home} />
-          <Stack.Screen name = "HowToUse" component = {HowToUse} />
-          <Stack.Screen name = "RegisterScreen" component = {RegisterScreen} />
-          <Stack.Screen name = "UpdateScreen" component = {UpdateProfile} />
-          <Stack.Screen name = "UpdatePassword" component = {UpdatePassword} />
-          <Stack.Screen name = "RatesScreen" component = {RatesScreen} />*/}
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
